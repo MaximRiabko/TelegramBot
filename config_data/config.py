72b53d4dc61b049118baf03a065e261e46d1713c
Maxim Riabko <maxim.riabko152@gmail.com>
2023-09-29 17:10:04 +0300

    Прописаны токены из файла .env.

--- config_data/config.py
+++ config_data/config.py
@@ -1,16 +1,14 @@
-# import os
-# # from dotenv import load_dotenv, find_dotenv
-# #
-# # if not find_dotenv():
-# #     exit("Переменные окружения не загружены т.к отсутствует файл .env")
-# # else:
-# #     load_dotenv()
+import os
+from dotenv import load_dotenv, find_dotenv
 
-# BOT_TOKEN = os.getenv("")
-# RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+if not find_dotenv():
+    exit("Переменные окружения не загружены т.к отсутствует файл .env")
+else:
+    load_dotenv()
 
-BOT_TOKEN = "6080375267:AAFVgZylGUKlhEUTkjIMSk1PxW2hPgDkDu4"
-RAPID_API_KEY = "37eee72770msh5ee7b17a443bcbbp15aa68jsnff3c3f46501c"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+RAPID_API_KEY = os.getenv("RAPID_API_KEY")
+
 DEFAULT_COMMANDS = (
     ("start", "Запустить бота"),
     ("help", "Вывести справку"),
